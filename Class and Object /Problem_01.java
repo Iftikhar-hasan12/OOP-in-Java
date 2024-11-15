2cd4a668bbce4a8495defb149101341144eb8181
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2024-11-15 15:12:37 +0600

    Update Problem_01.java

--- Class and Object /Problem_01.java
+++ Class and Object /Problem_01.java
@@ -1,1 +1,27 @@
 
+package com.mycompany.mid;
+
+class Person {
+
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+    void tv()
+    {
+        System.out.println("Name : "+name+ "\nAge : "+age);
+    }
+
+}
+
+public class Mid {
+
+    public static void main(String[] args) {
+        Person p = new Person("Atif",20);
+        p.tv();
+    }
+}
