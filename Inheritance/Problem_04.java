da079d3609ee9ece6341c3520e2a5eef8901c1e3
Iftikhar-hasan12 <iftikharhasan04@gmail.com>
2024-11-17 06:04:13 +0600

    Create Problem_04.java

---
+++ Inheritance/Problem_04.java
@@ -0,0 +1,44 @@
+/* Write a Java program to create a class called Employee with methods called work() and 
+getSalary(). Create a subclass called HRManager that 
+overrides the work() method and adds a new method called addEmployee().*/
+
+
+package com.mycompany.mid;
+
+class Employee{
+    
+    void work()
+    {
+        
+    }
+    double getSalary()
+    {
+        return 0;
+    }
+    
+    
+}
+
+class HRManager extends Employee{
+     
+      @Override
+      void work()
+    {
+        
+    }
+      
+      void AddnewEmployee()
+      {
+          
+      }
+      
+      
+}
+
+public class Mid {
+
+    public static void main(String[] args) {
+
+     
+    }
+}
